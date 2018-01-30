27f71dcaf8a7241828f382675ae0a839b8eaac12
Nikolaj Dyring <nikodyring@gmail.com>
2018-01-30 14:38:16 +0100

    MC Implementeret og testet.

---
+++ BilletLib/BilletLib/MC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BilletLib
+{
+    public class MC
+    {
+        public string Nummerplade { get; set; }
+        public DateTime Dato { get; set; }
+
+        public int Pris()
+        {
+            return 125;
+        }
+
+        public string Køretøj()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ BilletLib/UnitTestProject/MCTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BilletLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class MCTests
+    {
+        [TestMethod]
+        public void MCPrisEr125()
+        {
+            // Arrange
+            MC Suzuki = new MC();
+            // Act
+            int pris = Suzuki.Pris();
+            // Assert
+            Assert.AreEqual(125, pris);
+        }
+
+        [TestMethod]
+        public void MCKøretøjTest()
+        {
+            // Arrange
+            MC Honda = new MC();
+            // Act
+            string køretøj = Honda.Køretøj();
+            // Assert
+            Assert.AreEqual("MC", køretøj);
+        }
+    }
+}
