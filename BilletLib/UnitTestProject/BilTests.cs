d6c1df6c860d966af2305ce26fa072001f761edf
Nikolaj Dyring <nikodyring@gmail.com>
2018-01-30 14:29:09 +0100

    Opgave 2
    Oprettet en bil klasse, som har 2 simple metoder og 2 properties.

---
+++ BilletLib/UnitTestProject/BilTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BilletLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class BilTests
+    {
+        [TestMethod]
+        public void BilPris240Test()
+        {
+            // Arrange
+            Bil Opel = new Bil();
+            // Act
+            int pris = Opel.Pris();
+            // Assert
+            Assert.AreEqual(240, pris);
+        }
+
+        [TestMethod]
+        public void BilKøretøjTest()
+        {
+            // Arrange
+            Bil Toyota = new Bil();
+            // Act
+            string køretøj = Toyota.Køretøj();
+            // Assert
+            Assert.AreEqual("Bil", køretøj);
+        }
+    }
+}
